a2b3d05382de92d7640f5e644569f0dc7c55c95c
ReshmiMaulik <70208840+ReshmiMaulik@users.noreply.github.com>
2023-08-06 22:51:35 +0530

    Add files via upload

---
+++ Android-Journal-Paper Code.R
@@ -0,0 +1,66 @@
+library(lavaan)
+#Importing the csv data 
+
+setwd(" ") #Set the path of Working Directory
+
+data<-read.csv("workingdataA.csv " )
+#Set the path for the csv (database) file
+#Count of Rows and columns
+dim(data)
+View(data)
+# Print all variable names in a data frame
+names(data)
+
+df1 <- data[,c("ET","CC","CI","OE","OW","NI","SI","RI","FI")]
+
+# remove the first column from data matrix
+dfN <- df[, -1]
+
+#The difference in the magnitude between the variables is huge and it is suggested to scale the variables. 
+
+df1 <- apply(df1,  2, scale)
+
+
+summary(df1)
+
+modelA <- '
+             AC =~  CC + CI 
+            IDC =~   OE + 1* OW 
+            ITC =~   SI + NI + FI 
+            NI ~~ SI
+            SI ~~ FI
+            IDC ~~ AC
+            ITC ~~ CC
+            RI~  CI + SI
+            ET ~ AC + IDC + ITC + RI
+            '
+fitA <- sem(modelA, data=df1, std.lv=TRUE , estimator="DWLS")
+
+
+summary(fitA, standardized = TRUE, fit.measures = TRUE)
+
+fitMeasures(fitA, c("chisq","df","cfi", "rmsea", "srmr"))
+#chisq     df    cfi  rmsea   srmr 
+
+
+resid(fitA, "cor")
+
+library("semPlot")
+
+semPaths(fitA,what='std',
+         rotation = 4, equalizeManifests = FALSE, optimizeLatRes = TRUE, node.width = 1.5,
+         edge.width = 1, shapeMan = "rectangle", shapeLat = "ellipse", 
+         shapeInt = "triangle", sizeMan = 6, sizeInt = 6, sizeLat = 10, 
+         unCol = "#070b8c",
+         
+         node.label.cex=5,
+         edge.label.cex=1.25, curvePivot = TRUE, 
+         fade=FALSE)
+
+lavCor(fitA) #residuals
+
+inspect(fitA,'r2') # variance explained
+
+lavInspect(fitA, "cov.lv") #Covariance matrix of latent variables
+
+inspect(fitA,"sampstat")# sample means and covariance matrix
---
+++ Eclipse-Journal-Paper Code.R
@@ -0,0 +1,67 @@
+library(lavaan)
+#Importing the csv data 
+
+setwd(" ") #Set the path of Working Directory
+
+data<-read.csv("workingdataE.csv " )
+#Set the path for the csv (database) file
+#Count of Rows and columns
+dim(data)
+View(data)
+# Print all variable names in a data frame
+names(data)
+
+df1 <- data[,c("ET","CC","Pr","CI","OE","OW","NI","SI","RI","FI")]
+
+# remove the first column from data matrix
+dfN <- df[, -1]
+
+#The difference in the magnitude between the variables is huge and it is suggested to scale the variables. 
+
+df1 <- apply(df1,  2, scale)
+
+
+summary(df1)
+
+modelE<-'
+          AC =~  CC + CI + Pr
+          IDC =~   OE + OW + Pr
+          ITC =~   SI + NI + FI 
+          RI ~ CC + CI + OE
+          CC ~~ Pr
+          ET ~~ OW
+          OW ~~ CC
+          OE ~~ CI
+          ITC ~~ CC
+          ITC ~~ CI
+          ET ~  AC + IDC + ITC + RI'
+
+fitE <- sem(modelE, data=df1, std.lv=TRUE , estimator="DWLS")
+
+summary(fitE, standardized = TRUE, fit.measures = TRUE)
+
+fitMeasures(fitE, c("chisq","df","cfi", "rmsea", "srmr"))
+#chisq     df    cfi  rmsea   srmr 
+
+
+resid(fitE, "cor")
+
+library("semPlot")
+library("psych")
+semPaths(fitE,what='std',
+         rotation = 4, equalizeManifests = FALSE, optimizeLatRes = TRUE, node.width = 1.5,
+         edge.width = 1, shapeMan = "rectangle", shapeLat = "ellipse", 
+         shapeInt = "triangle", sizeMan = 6, sizeInt = 6, sizeLat = 10, 
+         unCol = "#070b8c",
+         
+         node.label.cex=5,
+         edge.label.cex=1.25, curvePivot = TRUE, 
+         fade=FALSE)
+
+lavCor(fitE) #residuals
+
+inspect(fitE,'r2') # variance explained
+
+lavInspect(fitE, "cov.lv") #Covariance matrix of latent variables
+
+inspect(fitE,"sampstat")# sample means and covariance matrix
